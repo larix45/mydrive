acffd232ee58d14b37b3766cf4c783d60a1846d8
Akr0nys <55049058+larix45@users.noreply.github.com>
2022-02-25 19:35:02 +0100

    Update choose-file.php

--- choose-file.php
+++ choose-file.php
@@ -9,20 +9,68 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Lato:wght@100&display=swap" rel="stylesheet">
     <script>
+        //const server_adress = "http://152.70.55.33/";
+        const server_adress = "http://localhost/";
+        function arrayRemove(arr, value) { 
+            return arr.filter(function(ele){ 
+                return ele != value; 
+            });
+        }
         function load()
         {
+            /*
+            <a href="asd.png">
+                <div class="element">
+                    <img class="fileicon" src="./mydrive-icons/512px/_blank.png"></img>
+                    <span class="filename">Plik.txt</span>
+                </div>
+            </a>
+            */
             console.log(JSON.parse(document.getElementById("files").value));
+            arrayRemove( arrayRemove(JSON.parse(document.getElementById("files").value),"."), "..").forEach(element => {
+                let a_link = document.createElement("a");
+                a_link.href = server_adress+"?filename="+element;
+                let div_element = document.createElement("div");
+                div_element.classList.add("element");
+                
+                let img_fileicon = document.createElement("img");
+                img_fileicon.src = "./mydrive-icons/512px/_blank.png";
+                img_fileicon.classList.add("fileicon");
+
+                let span_filename = document.createElement("span");
+                span_filename.classList.add("filename");
+                span_filename.innerText = element;
+
+                /**
+                 * TODO:
+                 * fileicons depending on file extension
+                 **/
+                div_element.appendChild(img_fileicon);
+
+                div_element.appendChild(span_filename);
+
+                a_link.appendChild(div_element);
+                console.log(element);
+                document.getElementsByTagName("section")[0].appendChild(a_link);
+            });
+            
         }
     </script>
     <style>
+       /*
+        @media (max-width: 1199px) { ... }
+
+        @media (max-width: 991px) { ... }
+
+        @media (max-width: 575px) { ... }*/
     body, html, section {
         margin:0px; 
         height: 100%;
     }
     section {
         display: grid;
-	    grid-template-columns: 12.5% 12.5% 12.5% 12.5% 12.5%  12.5% 12.5% 12.5%;
-	    grid-auto-rows: 33%;
+	    grid-template-columns: 12.5% 12.5% 12.5% 12.5% 12.5% 12.5% 12.5% 12.5%;
+	    grid-auto-rows: 25%;
         margin:1%;
     }
     #header {
@@ -33,9 +81,28 @@
         flex-direction: row;
         justify-content: center;
     }
+    .fileicon{
+        max-width:80%;
+        height:auto;
+    }
+    .filename{
+        font-size: large;
+        font-family: 'Lato', sans-serif;
+        font-weight:bolder;
+        text-align:center;
+        width:100%;
+        color:black;
+        word-wrap: break-word;
+    }
     .element {
-        background-color: #123123;
         margin:6%;
+        display:flex;
+        flex-direction:column;
+        align-items:center;
+        padding-top:7%;
+    }
+    .element:hover {
+        box-shadow: 0 0 5px 3px cornflowerblue;
     }
     #text {
         font-size: xx-large;
@@ -46,6 +113,9 @@
         margin:0px;
         padding: 1%;
     }
+    a   {
+        text-decoration:none;
+    }
     </style>
 </head>
 <body onload="load()">
@@ -55,140 +125,8 @@
         </div>
     </header>
     <section>
-        <div class="element">
-            1
-        </div>
-        <div class="element">
-            2
-        </div>
-        <div class="element">
-            3
-        </div>
-        <div class="element">
-            4
-        </div>
-        <div class="element">
-            5
-        </div>
-        <div class="element">
-            6
-        </div>
-        <div class="element">
-            7
-        </div>
-        <div class="element">
-            7
-        </div>
-        <div class="element">
-            7
-        </div>
-        <div class="element">
-            7
-        </div>
-        <div class="element">
-            7
-        </div>
-        <div class="element">
-            7
-        </div>
-        <div class="element">
-            7
-        </div>
-        <div class="element">
-            7
-        </div>
-        <div class="element">
-            7
-        </div>
-        <div class="element">
-            7
-        </div>
-        <div class="element">
-            7
-        </div>
-        <div class="element">
-            7
-        </div>
-        <div class="element">
-            7
-        </div>
-        <div class="element">
-            7
-        </div>
-        <div class="element">
-            7
-        </div>
-        <div class="element">
-            7
-        </div>
-        <div class="element">
-            7
-        </div>
-        <div class="element">
-            7
-        </div>
-        <div class="element">
-            7
-        </div>
-        <div class="element">
-            7
-        </div>
-        <div class="element">
-            7
-        </div>
-        <div class="element">
-            7
-        </div>
-        <div class="element">
-            7
-        </div>
-        <div class="element">
-            7
-        </div>
-        <div class="element">
-            7
-        </div>
-        <div class="element">
-            7
-        </div>
-        <div class="element">
-            7
-        </div>
-        <div class="element">
-            7
-        </div>
-        <div class="element">
-            7
-        </div>
-        <div class="element">
-            7
-        </div>
-        <div class="element">
-            7
-        </div>
-        <div class="element">
-            7
-        </div>  
-        <div class="element">
-            7
-        </div>
-        <div class="element">
-            7
-        </div>
-        <div class="element">
-            7
-        </div>
-        <div class="element">
-            7
-        </div>
-        <div class="element">
-            7
-        </div>
-        <div class="element">
-            7
-        </div>
     </section>
-
+    
     <?php
     echo "<input type='hidden' id='files' value='".json_encode(array_values(scandir("./public")))."'>";
     ?>
