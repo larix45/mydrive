99f49b86755005b5b5ba3188d1788d5a97cad0c7
Akr0nys <55049058+larix45@users.noreply.github.com>
2022-02-25 18:06:17 +0100

    Update choose-file.php

--- choose-file.php
+++ choose-file.php
@@ -1,3 +1,21 @@
-<?php
-phpinfo();
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script>
+        function load()
+        {
+            console.log(JSON.parse(document.getElementById("files").value)[0]);
+        }
+    </script>
+</head>
+<body load="load()">
+    <?php
+    echo "<input type='hidden' id='files' value='".json_encode(array_values(scandir("./public")))."'>";
+    ?>
+</body>
+</html>
+
